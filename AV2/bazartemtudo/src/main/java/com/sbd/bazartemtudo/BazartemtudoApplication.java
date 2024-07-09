2a8541d59dd4519b550b871cc95d0a7a6d996523
Cianeto <gbdopu02@gmail.com>
2024-07-09 06:49:49 -0300

    added enable scheduling annotation

--- AV2/bazartemtudo/src/main/java/com/sbd/bazartemtudo/BazartemtudoApplication.java
+++ AV2/bazartemtudo/src/main/java/com/sbd/bazartemtudo/BazartemtudoApplication.java
@@ -2,8 +2,10 @@ package com.sbd.bazartemtudo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class BazartemtudoApplication {
 
 	public static void main(String[] args) {
